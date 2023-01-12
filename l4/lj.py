6dff7522b204c1def71a0dd26ccbd3dae3b59f8b
Himanshu <himanshu@MacBook-Pro-14-inch-2077.local>
2023-01-12 16:24:14 +0530

    lecture 4 notes

---
+++ l4/lj.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+fig,ax = plt.subplots() 
+# Boltzmann's constant, J/K
+kB = 1.381e-23
+
+# The Lennard-Jones parameters:
+A = 1.024e-23   # J.nm^6
+B = 1.582e-26   # J.nm^12
+
+# Adjust the units of A and B - they have more manageable values
+# in K.nm^6 and K.nm^12
+A, B = A / kB, B / kB
+
+# Interatomic distance, in nm
+r = np.linspace(0.3, 1, 1000)
+# Interatomic potential
+U = B/r**12 - A/r**6
+# Interatomic force
+F = 12*B/r**13 - 6*A/r**7
+
+line1 = plt.plot(r, U, 'k', lw=2, label=r'U(r)')
+plt.xlim(0.3, 0.8)
+plt.ylim(-150, 100)
+
+plt.twinx()
+line2 = plt.plot(r, F, 'k', ls=':', lw=2, label=r'F(r)')
+plt.xlim(0.3, 0.8)
+plt.ylim(-1000, 1000)
+
+# Jump through some hoops to get the both line's labels in the same legend:
+lines = line1 + line2
+labels = []
+for line in lines:
+    labels.append(line.get_label())
+plt.legend(lines, labels)
+
+#plt.show()
+r = np.linspace(0.3, 1, 1000) 
+U = B/r**12 - A/r**6 
+ax.plot(r,U)
+fig.savefig('LJ.png', format='png',bbox_inches='tight',dpi=600)
+
